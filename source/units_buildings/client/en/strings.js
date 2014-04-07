58118890a442763409fe8c6e7763c8c567119a62
bryisazombie <bryanzettler@gmail.com>
2014-04-07 15:43:05 -0500

    files

---
+++ source/units_buildings/client/en/strings.js
@@ -0,0 +1,11 @@
+(function () {
+  "use strict";
+
+  var lang = XT.stringsFor("en_US", {
+    "_iceCreamFlavors": "Ice Cream Flavors"
+  });
+
+  if (typeof exports !== 'undefined') {
+    exports.language = lang;
+  }
+}());
