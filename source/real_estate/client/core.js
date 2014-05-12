96516c75caa51fb4c29b80390ef5b724c4d92fc8
Chris Hall <chall@pangeaequity.com>
2014-05-12 14:44:57 -0500

    Removed workspaces from panel list

--- source/real_estate/client/core.js
+++ source/real_estate/client/core.js
@@ -7,9 +7,6 @@
     panels: [
       {name: "realEstateBuildingList", kind: "XV.RealEstateBuildingList"},
       {name: "realEstateUnitList", kind: "XV.RealEstateUnitList"}
-    ], edit_views: [
-      {name: "realEstateBuildingEditList", kind: "XV.RealEstateBuildingEditList"},
-      {name: "realEstateUnitEditList", kind: "XV.RealEstateUnitEditList"}
     ]
   };
 }());
