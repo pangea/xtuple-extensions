e33c3f7b1f10e4d80618426e91746cb9d628835c
Chris Hall <chall@pangeaequity.com>
2014-05-14 13:28:10 -0500

    Added small support extension
    I really wanted to make this a part of the core xTuple repo, but adding
    new core extensions is a complete pain in the ass.  I lost an entire day
    to it

---
+++ source/support/client/core.js
@@ -0,0 +1,8 @@
+(function() {
+  "use strict";
+
+  XT.extensions.support = {
+    // HREF of the 'help link' in the upper left of the interface
+    helpLink: 'https://pangea.zendesk.com/forums'
+  };
+}());
---
+++ source/support/client/en/strings.js
@@ -0,0 +1,11 @@
+(function () {
+  "use strict";
+
+  var lang = XT.stringsFor("en_US", {
+    "_helpLink" : "Help Link"
+  });
+
+  if (typeof exports !== 'undefined') {
+    exports.language = lang;
+  }
+}());
