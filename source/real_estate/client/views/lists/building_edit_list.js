357c87e317817d1750002a231aebaeb74a6fca97
Bry Zettler <bryanzettler@gmail.com>
2014-06-23 22:22:20 -0500

    end of night

--- source/real_estate/client/views/lists/building_edit_list.js
+++ source/real_estate/client/views/lists/building_edit_list.js
@@ -8,28 +8,33 @@
       label: "_realEstateBuildings".loc(),
       collection: "XM.RealEstateBuildingCollection",
       components: [
-        {kind: "onyx.GroupboxHeader", content: "_overview".loc()},
-        {kind: "XV.ScrollableGroupbox", name: "mainGroup", classes: "in-panel", components: [
-          { kind: 'XV.InputWidget'    , attr: "buildingName" },
-          {kind: "XV.RealEstatePortfolioRelationWidget", attr: "portfolio"},
-          {kind: "XV.RealEstateZoneRelationWidget", attr: "zone"},
-          { kind: 'XV.NumberWidget'   , attr: "buildingTypeId" },
-          { kind: 'XV.NumberWidget'   , attr: "officeId" },
-          { kind: 'XV.NumberWidget'   , attr: "promoId" },
-          { kind: 'XV.NumberWidget'   , attr: "parkingVendorId" },
-          { kind: 'XV.DateWidget'     , attr: "assetAcquisitionDate" },
-          { kind: 'XV.DateWidget'     , attr: "assetSaleDate" },
-          { kind: 'XV.InputWidget'    , attr: "category" },
-          { kind: 'XV.NumberWidget'   , attr: "totalArea" },
-          { kind: 'XV.NumberWidget'   , attr: "floors" },
-          { kind: 'XV.InputWidget'    , attr: "address" },
-          { kind: 'XV.InputWidget'    , attr: "city" },
-          { kind: 'XV.InputWidget'    , attr: "state" },
-          { kind: 'XV.NumberWidget'   , attr: "zipCode" },
-          { kind: 'XV.InputWidget'    , attr: "neighborhood" },
-          { kind: 'XV.InputWidget'    , attr: "communityName" },
-          { kind: 'XV.CheckboxWidget' , attr: "leasingAgencyApproved" },
-          { kind: 'XV.NumberWidget'   , attr: "unitCount" }
+        {kind: "Panels", arrangerKind: "CarouselArranger",fit: true, components: [
+          {kind: "XV.Groupbox", name: "mainPanel", components: [
+            {kind: "onyx.GroupboxHeader", content: "_overview".loc()},
+            {kind: "XV.ScrollableGroupbox", name: "mainGroup", fit: true,
+             classes: "in-panel", components: [ 
+               { kind: 'XV.InputWidget'    , attr: "buildingName" },
+               {kind: "XV.RealEstatePortfolioRelationWidget", attr: "portfolio"},
+               {kind: "XV.RealEstateZoneRelationWidget", attr: "zone"},
+               { kind: 'XV.NumberWidget'   , attr: "buildingTypeId" },
+               { kind: 'XV.NumberWidget'   , attr: "officeId" },
+               { kind: 'XV.NumberWidget'   , attr: "promoId" },
+               { kind: 'XV.NumberWidget'   , attr: "parkingVendorId" },
+               { kind: 'XV.DateWidget'     , attr: "assetAcquisitionDate" },
+               { kind: 'XV.DateWidget'     , attr: "assetSaleDate" },
+               { kind: 'XV.InputWidget'    , attr: "category" },
+               { kind: 'XV.NumberWidget'   , attr: "totalArea" },
+               { kind: 'XV.NumberWidget'   , attr: "floors" },
+               { kind: 'XV.InputWidget'    , attr: "address" },
+               { kind: 'XV.InputWidget'    , attr: "city" },
+               { kind: 'XV.InputWidget'    , attr: "state" },
+               { kind: 'XV.NumberWidget'   , attr: "zipCode" },
+               { kind: 'XV.InputWidget'    , attr: "neighborhood" },
+               { kind: 'XV.InputWidget'    , attr: "communityName" },
+               { kind: 'XV.CheckboxWidget' , attr: "leasingAgencyApproved" },
+               { kind: 'XV.NumberWidget'   , attr: "unitCount" }
+             ]}
+          ]}
         ]}
       ]
     });
--- source/real_estate/client/views/workspaces/lease_workspace.js
+++ source/real_estate/client/views/workspaces/lease_workspace.js
@@ -8,28 +8,27 @@
       title: "_realEstateLease".loc(),
       model: "XM.RealEstateLease",
       components: [
-        {kind: "Panels", arrangerKind: "CarouselArranger",
-         fit: true, components: [
-           {kind: "XV.ScrollableGroupbox", name: "mainPanel", components: [
-             {kind: "onyx.GroupboxHeader", content: "_overview".loc()},
-             {kind: "XV.ScrollableGroupbox", name: "mainGroup", fit: true,
-              classes: "in-panel", components: [ 
-                { kind: 'XV.NumberWidget', attr: "unitId" },
-                { kind: 'XV.NumberWidget', attr: "crmacctId" },
-                { kind: "XV.DateWidget", attr: "startDate" },
-                { kind: "XV.DateWidget", attr: "endDate" },
-                { kind: "XV.DateWidget", attr: "moveInDate" },          
-                { kind: "XV.DateWidget", attr: "vacatedDate" },
-                { kind: "XV.InputWidget", attr: "reasonForLeaving" },
-                { kind: "XV.DateWidget", attr: "scheduledMoveOutDate" },
-                { kind: "XV.InputWidget", attr: "status" },
-                { kind: "XV.InputWidget", attr: "publicAssistanceProgram" },
-                { kind: "XV.CheckboxWidget", attr: "movedWithin" },
-                { kind: "XV.DateWidget", attr: "createdAt" },
-                { kind: 'XV.LeaseTypePicker', attr: "type" }
-              ]}
-           ]}
-         ]}
+        {kind: "Panels", arrangerKind: "CarouselArranger",fit: true, components: [
+          {kind: "XV.Groupbox", name: "mainPanel", components: [
+            {kind: "onyx.GroupboxHeader", content: "_overview".loc()},
+            {kind: "XV.ScrollableGroupbox", name: "mainGroup", fit: true,
+             classes: "in-panel", components: [ 
+               { kind: 'XV.NumberWidget', attr: "unitId" },
+               { kind: 'XV.NumberWidget', attr: "crmacctId" },
+               { kind: "XV.DateWidget", attr: "startDate" },
+               { kind: "XV.DateWidget", attr: "endDate" },
+               { kind: "XV.DateWidget", attr: "moveInDate" },          
+               { kind: "XV.DateWidget", attr: "vacatedDate" },
+               { kind: "XV.InputWidget", attr: "reasonForLeaving" },
+               { kind: "XV.DateWidget", attr: "scheduledMoveOutDate" },
+               { kind: "XV.InputWidget", attr: "status" },
+               { kind: "XV.InputWidget", attr: "publicAssistanceProgram" },
+               { kind: "XV.CheckboxWidget", attr: "movedWithin" },
+               { kind: "XV.DateWidget", attr: "createdAt" },
+               { kind: 'XV.LeaseTypePicker', attr: "type" }
+             ]}
+          ]}
+        ]}
       ]
     });
 
