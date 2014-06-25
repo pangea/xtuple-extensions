251a4cad549fa08c65b603c2ad05fde81cbc76cb
Mark McCanna <markmccanna1@gmail.com>
2014-06-25 10:33:28 -0500

    added billing strings.js, database side of billing added

--- source/accounting/database/source/manifest.js
+++ source/accounting/database/source/manifest.js
@@ -1,7 +1,22 @@
 {
   "name": "accounting",
-  "version": "0.1.0",
   "comment": "accounting extension",
-  "loadOrder": 999,
-  "databaseScripts": []
+  "loadOrder": 30,
+  "databaseScripts": [
+    //billing
+    "xt/tables/rptdef.sql",
+    "xm/javascript/billing.sql",
+    "xm/javascript/cashrcpt.sql",
+    "xm/javascript/invoice.sql",
+    "xm/javascript/return.sql",
+    "xm/javascript/receivable.sql",
+    "xm/javascript/sales_category.sql",
+    "xt/functions/ar_balance.sql",
+    "xt/functions/ar_tax_total.sql",
+    "xt/functions/cashrcpt.sql",
+    "xt/views/receivable_invoice_return.sql",
+    "xt/views/aropeninfo.sql",
+    "xt/views/receivable_applications.sql",
+    "xt/views/cashrcpt.sql"
+  ]
 }
